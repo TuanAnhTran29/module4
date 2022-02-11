5ec488e2ae57fd08dae68b51a64cfc0ff010d067
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-02-11 16:30:37 +0700

    Added folder PhanQuyenDuaTrenVaiTro

---
+++ PhanQuyenDuaTrenVaiTro/src/main/java/com/configuration/AppInit.java
@@ -0,0 +1,22 @@
+package com.configuration;
+
+import com.sun.org.apache.xerces.internal.parsers.SecurityConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
