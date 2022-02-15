724c6efe81abda618e1759e47c593bde475153d1
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-02-15 17:01:05 +0700

    Added folder SpringBootXuLyException

---
+++ SpringBootXuLyException/src/main/java/com/springbootxulyexception/SpringBootXuLyExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.springbootxulyexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootXuLyExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootXuLyExceptionApplication.class, args);
+    }
+
+}
---
+++ SpringBootXuLyException/src/test/java/com/springbootxulyexception/SpringBootXuLyExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootxulyexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootXuLyExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
