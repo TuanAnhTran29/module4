92ca79a85eecfc4d650b2069b83eca25989f07d2
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-02-16 23:34:26 +0700

    Added folder MigrateWebAppSuDungSpringBoot

---
+++ MigrateWebAppSuDungSpringBoot/src/main/java/cg/wbd/grandemonstration/GranDemonstrationApplication.java
@@ -0,0 +1,9 @@
+package cg.wbd.grandemonstration;
+
+@SpringBootApplication
+public class GranDemonstrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranDemonstrationApplication.class,args);
+    }
+}
