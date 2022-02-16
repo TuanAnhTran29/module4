77f0e1073621ae784bb7483f9ef677260aad3a79
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-02-16 23:18:38 +0700

    Added folder SpringBootJSONWebToken

---
+++ SpringBootJSONWebToken/src/main/java/com/springbootjsonwebtoken/SpringBootJsonWebTokenApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjsonwebtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonWebTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJsonWebTokenApplication.class, args);
+    }
+
+}
---
+++ SpringBootJSONWebToken/src/main/java/com/springbootjsonwebtoken/controller/UserRestController.java
@@ -0,0 +1,72 @@
+package com.springbootjsonwebtoken.controller;
+
+
+import com.springbootjsonwebtoken.entities.User;
+import com.springbootjsonwebtoken.service.JwtService;
+import com.springbootjsonwebtoken.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest")
+public class UserRestController {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll(){
+        return new ResponseEntity<List<User>>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUserById(@PathVariable int id){
+        User user= userService.findById(id);
+        if (user != null){
+            return new ResponseEntity<User>(user,HttpStatus.FOUND);
+        }else {
+            return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        if (userService.add(user)) {
+            return new ResponseEntity<String>("Created!", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<String>("User Existed!", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteUserById(@PathVariable int id) {
+        userService.delete(id);
+        return new ResponseEntity<String>("Deleted!", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<String> login(HttpServletRequest request, @RequestBody User user){
+        String result= "";
+        HttpStatus httpStatus= null;
+        try {
+            if (userService.checkLogin(user)){
+                result= jwtService.generateTokenLogin(user.getUsername());
+                httpStatus= HttpStatus.OK;
+            }else {
+                result= "Wrong userId and password";
+                httpStatus = HttpStatus.BAD_REQUEST;
+            }
+        } catch (Exception e) {
+            result= "Server Error";
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<String>(result,httpStatus);
+    }
+}
