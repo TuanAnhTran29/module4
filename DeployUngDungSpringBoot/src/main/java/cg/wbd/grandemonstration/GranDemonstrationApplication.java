cee679055d3e231ba912ca624faf205aff49c610
TuanAnhTran29 <tuananhtranquoc@gmail.com>
2022-02-21 18:00:49 +0700

    Added folder DeployUngDungSpringBoot

---
+++ DeployUngDungSpringBoot/src/main/java/cg/wbd/grandemonstration/GranDemonstrationApplication.java
@@ -0,0 +1,20 @@
+package cg.wbd.grandemonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class GranDemonstrationApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranDemonstrationApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GranDemonstrationApplication.class);
+    }
+}
+
